217701acdcd7996a16d4d34ef9895b1e4772c810
Jean Pimentel <contato@jeanpimentel.com.br>
2015-05-07 15:37:08 -0300

    Lets go again

---
+++ HonourBridge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HonourBridge
+//
+//  Created by Jean Pimentel on 5/7/15.
+//  Copyright (c) 2015 Jean Pimentel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HonourBridge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HonourBridge
+//
+//  Created by Jean Pimentel on 5/7/15.
+//  Copyright (c) 2015 Jean Pimentel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
